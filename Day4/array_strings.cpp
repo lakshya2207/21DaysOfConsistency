c2b7defcdfa893f632ad9fe67cc930864834db7b
Lakshya Sharma <lakshyasharma1928@gmail.com>
2025-02-01 06:34:54 +0530

    Create array_strings.cpp

---
+++ Day4/array_strings.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+    cout << "Hellow world";
+    int arr[10][10];
+    // cin>>arr[0][1];//>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    // cout << arr[0][1] <<" \n";
+    cout << arr[1][1];
+    return 0;
+}
